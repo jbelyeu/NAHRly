a944264435858931db5f9088878b3d0e5563b866
Jonathan Belyeu <jrbelyeu@gmail.com>
2019-04-11 16:49:55 -0600

    asynchonously counts reads in regions of bam files

--- src/count.py
+++ src/count.py
@@ -3,6 +3,7 @@ from __future__ import print_function
 import warnings
 warnings.filterwarnings("ignore", message="numpy.dtype size changed")
 warnings.filterwarnings("ignore", message="numpy.ufunc size changed")
+import numpy as np
 import pandas as pd
 import sys
 import argparse
@@ -13,6 +14,9 @@ import matplotlib
 matplotlib.use('agg')
 import matplotlib.pyplot as plt
 import pysam
+import signal
+import time 
+from multiprocessing import Pool
 
 #arg parsing
 ######################################################################################################################
@@ -22,39 +26,58 @@ parser.add_argument("-b", "--bed",
     help="bedfile of NAHR-prone regions",
     required=True
 )
-parser.add_argument("-o", "--out_dir", 
-    help="directory for output",
+parser.add_argument("-c", "--count_file",
+    help="filename for output",
     required=True
+)
+parser.add_argument("-P", "--process_count",
+    help="number of processes to use",
+    required=False,
+    type=int,
+    default=1
 )
 
 args = parser.parse_args()
 
-
-
 #####################################################################################################################
-
 #functions and generators
 ######################################################################################################################
-
-######################################################################################################################
-
-#main block
-######################################################################################################################
+#this must be global for function access
 regions = []
 with open(args.bed, 'r') as regions_file:
     for line in regions_file:
         regions.append(line.split()[:3])
 
-data = pd.DataFrame()
-#TODO integrate this into the process for streamlined counting of reads in the samples/regions of interest
-for cram in glob.glob(os.path.join(args.cram_dir,"*.cram")):
-    sample = os.path.splitext(cram)[0]
-    samfile = pysam.AlignmentFile(cram, "rc")
+def count_sample(cram):
+    sample = os.path.split(os.path.splitext(cram)[0])[-1]
     region_counts = []
-
-
     for region in regions:
+        samfile = pysam.AlignmentFile(cram, "rc")
         iter = samfile.fetch(region[0], int(region[1]), int(region[2]))
         region_counts.append(sum(1 for x in iter if x.mapping_quality > 0 ))
-    data[sample] = pd.Series(np.array(region_counts), index=regions)
-data.to_csv("readcounts.csv")
+    countseries_pair = sample,pd.Series(np.array(region_counts), index=["_".join(region) for region in regions])
+    return countseries_pair
+    
+######################################################################################################################
+
+#main block
+######################################################################################################################
+original_sigint_handler = signal.signal(signal.SIGINT, signal.SIG_IGN)
+pool=Pool(processes=args.process_count)
+signal.signal(signal.SIGINT, original_sigint_handler)
+try:
+    crams = glob.glob(os.path.join(args.cram_dir,"*.cram"))
+    result = pool.map_async(count_sample, (cram for cram in crams))
+    while not result.ready():
+        time.sleep(1)
+except KeyboardInterrupt:
+    print("Keyboard interrupt")
+    pool.terminate()
+counts = result.get()
+pool.close()
+pool.join()
+
+data = pd.DataFrame()
+for sample,count in counts:
+    data[sample] = count
+data.to_csv(args.count_file)
