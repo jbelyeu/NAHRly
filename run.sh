9ffbceececaa5aec49b90415d1c75cebc0baf4c9
Jonathan Belyeu <jrbelyeu@gmail.com>
2019-04-10 18:34:10 -0600

    updated run, decrement soon

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #run per-sample in ceph, current example is just one sample
-for filename in /scratch/ucgd/lustre/u1006375/ceph/ceph-bams/*.bam; do
+crams="/scratch/ucgd/lustre/UCGD_Datahub/Repository/AnalysisData/2016/A414/16-08-06_WashU-Yandell-CEPH/UGP/Data/PolishedBams"
+for filename in $crams/*.cram; do
     sample=$(echo "$filename" | grep -oP "\d+.bam" | grep -oP "\d+")
     echo "mosdepth -n --by ../betwixt_superdups.bed output/$sample $filename"
 done
